6f0eb8f506f7fff0e8377fc6fa305bc400e4e1c1
Mathis Chambon <mathis@MacBook-Pro-de-Mathis-4.local>
2024-05-28 18:25:39 +0200

    feat: update / delete

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import { Todo } from "./components/Todo";
 const App = () => {
   const [todos, setTodos] = useState([
     {
+      id: 1,
       name: "Faire les courses",
       done: true,
     },
     {
+      id: 2,
       name: "Faire le ménage",
       done: false,
     },
     {
+      id: 3,
       name: "Faire du sport",
       done: false,
     },
@@ -23,11 +26,17 @@ const App = () => {
     <div className="flex justify-center items-center gap-8 flex-col">
       <h1 className="text-4xl font-bold text-blue-500 text-center my-4">Ma todo list</h1>
 
-      <ul className="flex flex-col gap-2 w-1/3">
-        {todos.map((todo, index) => (
-          <Todo key={index} todo={todo} />
-        ))}
-      </ul>
+      {todos.length === 0 ? (
+        <span className="font-bold text-lg text-center">
+          Vous n&apos;avez pas ajouté de todo !
+        </span>
+      ) : (
+        <ul className="flex flex-col gap-2 w-1/3">
+          {todos.map((todo, index) => (
+            <Todo key={index} todo={todo} setTodos={setTodos} />
+          ))}
+        </ul>
+      )}
 
       <div className="flex gap-2 w-1/3">
         <input
@@ -40,15 +49,17 @@ const App = () => {
         <button
           className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-400 transition-colors duration-150"
           onClick={() => {
-            setTodos([
-              ...todos,
-              {
-                name: value,
-                done: false,
-              },
-            ]);
+            if (value !== "") {
+              setTodos([
+                ...todos,
+                {
+                  name: value,
+                  done: false,
+                },
+              ]);
 
-            setValue("");
+              setValue("");
+            }
           }}
         >
           Ajouter
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,37 @@
-export const Todo = ({ todo }) => {
+export const Todo = ({ todo, setTodos }) => {
   return (
-    <li className="px-4 py-2 border rounded-md bg-gray-200 flex justify-between">
+    <li className="px-4 py-2 border rounded-md bg-gray-200 flex justify-between items-center">
       <span>{todo.name}</span>
-      <button className="text-white bg-red-500 px-2 py-1 rounded-md hover:bg-red-400 transition-colors duration-150">
-        Supprimer
-      </button>
+
+      <div className="flex gap-2">
+        <button
+          onClick={() => {
+            setTodos((previousTodos) =>
+              previousTodos.map((currentTodo) => {
+                if (currentTodo.id === todo.id) {
+                  return {
+                    ...currentTodo,
+                    done: !currentTodo.done,
+                  };
+                }
+              })
+            );
+          }}
+          className="text-white bg-green-500 px-2 py-1 rounded-md hover:bg-green-400 transition-colors duration-150"
+        >
+          Marqué comme {todo.done ? "non fait" : "fait"}
+        </button>
+        <button
+          onClick={() => {
+            setTodos((previousTodos) =>
+              previousTodos.filter((prev) => todo.id !== prev.id)
+            );
+          }}
+          className="text-white bg-red-500 px-2 py-1 rounded-md hover:bg-red-400 transition-colors duration-150"
+        >
+          Supprimer
+        </button>
+      </div>
     </li>
   );
 };
